950aa20ae02127e6a7f05629be09643c65622dd6
pradeepkumar <android@qandle.com>
2021-01-23 19:17:41 +0530

    first

---
+++ app/src/main/java/com/pradeep/stockapp/Splash.java
@@ -0,0 +1,29 @@
+package com.pradeep.stockapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        continueLogin();
+    }
+
+
+    private void continueLogin()
+    {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                finish();
+            }
+        },500);
+
+    }
+}
